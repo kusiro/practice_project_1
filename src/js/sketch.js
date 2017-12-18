5a2523407b2c02778b06aa4dd57465b1af0bcd9c
Unknown <jackysoon871008@gmail.com>
2017-12-18 18:30:06 +0800

    初始化

--- src/js/sketch.js
+++
@@ -1,7 +0,0 @@
-function setup() {
-    createCanvas(480,480);
-}
-function draw() {
-    background(30);
-    rect(50,50,40,40);
-}
--- src/main.js
+++ src/main.js
@@ -1,3 +1,2 @@
-import processing from './js/p5.min.js';
 import html from '../index.html';
 import style from './style/style.scss';
