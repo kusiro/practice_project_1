54908f1bb7936ac66a8762bc7174f3b4e4b224ef
Unknown <jackysoon871008@gmail.com>
2017-12-18 18:15:14 +0800

    project set up

---
+++ src/main.js
@@ -0,0 +1,3 @@
+import processing from './js/p5.min.js';
+import html from '../index.html';
+import style from './style/style.scss';
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: './src/main.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                loader: 'html-loader',
+            },{
+                test: /\.js$/,
+                exclude: /(node_module)|(bower_components)/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['ES2015'],
+                    plugins: [],
+                }
+            },{
+                test: /\.scss|css$/,
+                use: [
+                    {loader: "style-loader"},
+                    {loader: "css-loader"},
+                    {loader: "sass-loader"},
+                ]
+            }
+        ]
+    },
+    devServer: {
+        inline: true,
+        compress: true,
+        hot: true,
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+    ],
+};
